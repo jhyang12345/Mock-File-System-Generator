c1deeeaa1a8c31abf6209b7c63fa3b6a1d3c9bf7
Jae-Hyeong Yang <jhyang12345@naver.com>
2019-04-22 09:37:08 +0900

    Added directory path reducer

--- util.py
+++ util.py
@@ -5,3 +5,9 @@ def create_and_return_directory(directory):
     if not os.path.exists(directory):
         os.makedirs(directory)
     return directory
+
+def reduce_file_path(path_list):
+    ret = ""
+    for p in path_list:
+        ret = os.path.join(ret, p)
+    return ret
