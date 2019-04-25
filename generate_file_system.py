11c314c3106a350de97bf3f33036dd830eae43d3
Jae-Hyeong Yang <jhyang12345@naver.com>
2019-04-25 20:46:29 +0900

    Created file generating function

--- generate_file_system.py
+++ generate_file_system.py
@@ -1,5 +1,6 @@
 import os, random, time
 from datetime import datetime
+from pathlib import Path
 
 def generate_mod_time(count=100, start='2017-01-01', end=None):
     start_time = time.mktime(datetime.strptime(start, "%Y-%m-%d").timetuple())
@@ -25,11 +26,29 @@ def read_words():
 
 def generate_file_names(count=100):
     words = read_words()
+    ret = []
     for _ in range(count):
         word_count = random.randint(1, 4)
         name_words = random.choices(words, k=word_count)
-        print(name_words)
+        ret.append(" ".join(name_words))
+    return ret
+
+def generate_files(destination, count=100, is_directory=False):
+    file_names = generate_file_names(count)
+    mod_times = generate_mod_time(count)
+    for i in range(count):
+        file_name = os.path.join(destination, file_names[i])
+        mod_time = time.mktime(mod_times[i].timetuple())
+        if is_directory:
+            os.makedirs(file_name)
+        else:
+            file_name = "%s.txt" % file_name
+            with open(file_name, "w") as f:
+                f.write("")
+        os.utime(file_name, (mod_time, mod_time))
+        print("Generated file:", file_name)
+    
 
 if __name__ == '__main__':
-    generate_file_names()
-    generate_mod_time()
+    default_dir = os.path.join(Path.home(), "Documents/test_directory")
+    generate_files(default_dir)
